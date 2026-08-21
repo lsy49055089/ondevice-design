bdc75cb1efaa4ce6fd1a7bd04e10a8c6af3eb30c
이승열 <tmdduf1101@naver.com>
2026-08-21 18:47:19 +0900

    Add RV32I simulation testbench

---
+++ fpga-rv32i-single-cycle/tb/tb_rv32i.sv
@@ -0,0 +1,23 @@
+`timescale 1ns / 1ps
+
+
+module tb_rv32i ();
+
+
+    logic clk, rst;
+    top_rv32i_soc dut (.*);
+
+    always #5 clk = ~clk;
+    initial begin
+        clk = 0;
+        rst = 1;
+
+        @(negedge clk);
+        @(negedge clk);
+
+        rst = 0;
+        repeat(1100) @(negedge clk);
+        $stop;
+    end
+
+endmodule
